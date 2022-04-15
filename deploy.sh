cb8f9be655b913c635f3b36ceecc25a29d748ceb
Behemoth11 <kodohdaniel@gmail.com>
2022-04-15 15:15:49 -0500

    - removed dummy sample.txt

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 
+# builing the go project
+go build ./cmd/api
 
+# building the frontend with angular and scully
+cd angular && ng build && npx scully && cd ../
+
+# deployment to heroku 
 heroku container:push web &&
 heroku container:release web &&
 heroku logs --tail
