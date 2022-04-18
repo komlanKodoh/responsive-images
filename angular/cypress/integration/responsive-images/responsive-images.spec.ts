8f0dee07af421deda55e81578154d264bd40e4a9
Behemoth11 <kodohdaniel@gmail.com>
2022-04-18 10:11:38 -0500

    - added some itegration test

--- angular/cypress/integration/responsive-images/responsive-images.spec.ts
+++ angular/cypress/integration/responsive-images/responsive-images.spec.ts
@@ -1,10 +1,15 @@
 /// <reference types="cypress" />
 
-import { assertNever } from "@scullyio/scully/src/lib/utils";
+import { assertNever } from '@scullyio/scully/src/lib/utils';
 
 describe('responsive-image optimization', () => {
   beforeEach(() => {
     cy.visit('http://localhost');
+
+    cy.intercept({
+      method: 'POST',
+      url: '/api/image',
+    }).as('optimize');
   });
 
   it('Get started brings form into view', () => {
@@ -20,33 +25,52 @@ describe('responsive-image optimization', () => {
 
   it('File upload should work', () => {
     cy.get('input[type=file]').attachFile('image_1mb.jpg');
-  })
+  });
 
   it('File upload should fail is file is greater than 5mb', () => {
     cy.get('input[type=file]').attachFile('image_5mb.jpg');
 
-    cy.contains("5 mb").should('be.visible')
-  })
+    cy.contains('5 mb').should('be.visible');
+  });
 
-  // it("Should send proper response after upload" , () => {
+  it('Should send proper response after upload', () => {
+    cy.get('input[type=file]').attachFile('image_1mb.jpg');
 
-  //   cy.intercept({
-  //     method: 'POST',
-  //     url: '/*',
-  //   }).as('optimize')
+    // This time out is important because there is the delay between the file upload and the time at which the buttons is clickable;
+    cy.wait(100);
+    cy.contains('button', 'Optimize').click();
 
-  //   cy.get('input[type=file]').attachFile('image_1mb.jpg');
-  //   cy.get('button').contains("Optimize").click();
+    cy.wait('@optimize').then((interception) => {
+      let response = interception.response;
+      assert.isDefined(response);
 
-  //   cy.wait('@optimize', {timeout: 1000000}).then((interception) => {
-  //     let response = interception.response;
-  //     assert.isDefined(response);
+      assert.equal(response?.statusCode, 200);
 
-  //     assert.equal(response?.statusCode, 200);
+      assert.isDefined(response?.body?.data?.placeholder);
+      assert.isDefined(response?.body?.data?.id);
+    });
+  });
 
-  //     assert.isDefined(response?.body?.data?.placeholder);
-  //     assert.isDefined(response?.body?.data?.id);
-  //   })
-  // })
+  it('File should be accessible after upload', () => {
+    cy.get('input[type=file]').attachFile('image_1mb.jpg');
 
+    cy.wait(100);
+    cy.get('button')
+      .contains(/optimize/i)
+      .click();
+
+    cy.wait('@optimize');
+
+    cy.contains('a', /download/i)
+      .should('exist')
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.log(href || ' ');
+        assert.isDefined(href);
+
+        cy.request(href as string)
+          .its('status')
+          .should('eq', 200);
+      });
+  });
 });
